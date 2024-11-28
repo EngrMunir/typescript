ffba7312ad0ad7dfcd341b4259c64463572df535
munir <smsirajulmonir@gmail.com>
2024-11-28 09:40:19 +0600

    generic with interface practiced

--- 2.4.ts
+++ 2.4.ts
@@ -1,3 +1,52 @@
 {
     // generic with interface
+
+    interface Developer<T> {
+        name: string;
+        computer:{
+            brand: string;
+            model: string;
+            releaseYear: number
+        }
+        smartWatch: T
+    }
+
+    type EmilabWatch ={
+        brand: string;
+        model: string;
+        display: string;
+    }
+
+    const poorDeveloper : Developer<EmilabWatch> ={
+        name:'munir',
+        computer:{
+            brand:'hp',
+            model:'x-335R',
+            releaseYear: 2023
+        },
+        smartWatch:{
+            brand:'Emailab',
+            model:'kw6',
+            display:'oled'
+        }
+    }
+
+    interface AppleWatch {
+        brand: string;
+        model: string;
+        heartTrack: boolean
+    }
+    const richDeveloper : Developer<AppleWatch> ={
+        name:'shivlo',
+        computer:{
+            brand:'hp',
+            model:'x-335R',
+            releaseYear: 2023
+        },
+        smartWatch:{
+            brand:'Apple',
+            model:'something',
+            heartTrack:true
+        }
+    }
 }
